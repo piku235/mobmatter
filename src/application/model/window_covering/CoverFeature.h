63cb44ed08b3b5f0325e600573f31f2e5335c7eb
Piotr Kugla <piku235@gmail.com>
2025-08-06 09:31:53 +0200

    Drop codes from CoverFeature

--- src/application/model/window_covering/CoverFeature.h
+++ src/application/model/window_covering/CoverFeature.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <cstdint>
-
 namespace mmbridge::application::model::window_covering {
 
-enum class CoverFeature : uint8_t {
-    Lift = 0x1,
-    PositionAwareLift = 0x2,
-    EdgePositionAwareLift = 0x4, // non existing in matter
+enum class CoverFeature {
+    Lift,
+    PositionAwareLift,
+    EdgePositionAwareLift, // non existing in matter
     // todo: tilt in later development
 };
 
