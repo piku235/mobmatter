a95e4ee25139b2e9a055569540574933d364109f
Piotr Kugla <piku235@gmail.com>
2025-08-09 17:06:39 +0200

    Fix code style in PercentTest

--- tests/application/model/PercentTest.cpp
+++ tests/application/model/PercentTest.cpp
@@ -4,94 +4,105 @@
 
 using mmbridge::application::model::Percent;
 
-TEST(PercentTest, Max) {
-  auto percent = Percent::max();
+TEST(PercentTest, Max)
+{
+    auto percent = Percent::max();
 
-  EXPECT_EQ(10000, percent.value100ths());
-  EXPECT_EQ(100, percent.value());
+    EXPECT_EQ(10000, percent.value100ths());
+    EXPECT_EQ(100, percent.value());
 }
 
-TEST(PercentTest, Min) {
-  auto percent = Percent::min();
+TEST(PercentTest, Min)
+{
+    auto percent = Percent::min();
 
-  EXPECT_EQ(0, percent.value100ths());
-  EXPECT_EQ(0, percent.value());
+    EXPECT_EQ(0, percent.value100ths());
+    EXPECT_EQ(0, percent.value());
 }
 
-TEST(PercentTest, ConstructsFromValue) {
-  auto percent = Percent::from(23);
+TEST(PercentTest, ConstructsFromValue)
+{
+    auto percent = Percent::from(23);
 
-  EXPECT_TRUE(percent.has_value());
-  EXPECT_EQ(2300, percent->value100ths());
-  EXPECT_EQ(23, percent->value());
+    EXPECT_TRUE(percent.has_value());
+    EXPECT_EQ(2300, percent->value100ths());
+    EXPECT_EQ(23, percent->value());
 }
 
-TEST(PercentTest, ConstructFailsFromValue) {
-  auto percent = Percent::from(101);
+TEST(PercentTest, ConstructFailsFromValue)
+{
+    auto percent = Percent::from(101);
 
-  EXPECT_FALSE(percent.has_value());
+    EXPECT_FALSE(percent.has_value());
 }
 
-TEST(PercentTest, ConstructsFromValue100ths) {
-  auto percent = Percent::from100ths(2345);
+TEST(PercentTest, ConstructsFromValue100ths)
+{
+    auto percent = Percent::from100ths(2345);
 
-  EXPECT_TRUE(percent.has_value());
-  EXPECT_EQ(2345, percent->value100ths());
-  EXPECT_EQ(23, percent->value());
+    EXPECT_TRUE(percent.has_value());
+    EXPECT_EQ(2345, percent->value100ths());
+    EXPECT_EQ(23, percent->value());
 }
 
-TEST(PercentTest, ConstructFailsFromValue100ths) {
-  auto percent = Percent::from100ths(10100);
+TEST(PercentTest, ConstructFailsFromValue100ths)
+{
+    auto percent = Percent::from100ths(10100);
 
-  EXPECT_FALSE(percent.has_value());
+    EXPECT_FALSE(percent.has_value());
 }
 
-TEST(PercentTest, AddsAnother) {
-  auto percent = Percent::from(10);
-  auto other = Percent::from100ths(1234);
+TEST(PercentTest, AddsAnother)
+{
+    auto percent = Percent::from(10);
+    auto other = Percent::from100ths(1234);
 
-  auto actual = *percent + *other;
+    auto actual = *percent + *other;
 
-  EXPECT_EQ(2234, actual.value100ths());
-  EXPECT_EQ(22, actual.value());
+    EXPECT_EQ(2234, actual.value100ths());
+    EXPECT_EQ(22, actual.value());
 }
 
-TEST(PercentTest, AddOverflow) {
-  auto percent = Percent::from(90);
-  auto other = Percent::from100ths(1234);
+TEST(PercentTest, AddOverflow)
+{
+    auto percent = Percent::from(90);
+    auto other = Percent::from100ths(1234);
 
-  auto actual = *percent + *other;
+    auto actual = *percent + *other;
 
-  EXPECT_EQ(10000, actual.value100ths());
-  EXPECT_EQ(100, actual.value());
+    EXPECT_EQ(10000, actual.value100ths());
+    EXPECT_EQ(100, actual.value());
 }
 
-TEST(PercentTest, SubsAnother) {
-  auto percent = Percent::from100ths(1234);
-  auto other = Percent::from(10);
+TEST(PercentTest, SubsAnother)
+{
+    auto percent = Percent::from100ths(1234);
+    auto other = Percent::from(10);
 
-  auto actual = *percent - *other;
+    auto actual = *percent - *other;
 
-  EXPECT_EQ(234, actual.value100ths());
-  EXPECT_EQ(2, actual.value());
+    EXPECT_EQ(234, actual.value100ths());
+    EXPECT_EQ(2, actual.value());
 }
 
-TEST(PercentTest, SubsOverflow) {
-  auto percent = Percent::from(10);
-  auto other = Percent::from100ths(1234);
+TEST(PercentTest, SubsOverflow)
+{
+    auto percent = Percent::from(10);
+    auto other = Percent::from100ths(1234);
 
-  auto actual = *percent - *other;
+    auto actual = *percent - *other;
 
-  EXPECT_EQ(0, actual.value100ths());
-  EXPECT_EQ(0, actual.value());
+    EXPECT_EQ(0, actual.value100ths());
+    EXPECT_EQ(0, actual.value());
 }
 
-TEST(PercentTest, Compare) {
-  EXPECT_EQ(Percent::from(10), Percent::from(10));
-  EXPECT_EQ(Percent::from(10), Percent::from100ths(1000));
-  EXPECT_NE(Percent::from(10), Percent::from100ths(1001));
-  EXPECT_LT(Percent::from(10), Percent::from100ths(1001));
-  EXPECT_LE(Percent::from(10), Percent::from100ths(1000));
-  EXPECT_GT(Percent::from100ths(1001), Percent::from(10));
-  EXPECT_GE(Percent::from100ths(1000), Percent::from(10));
+TEST(PercentTest, Compare)
+{
+    EXPECT_EQ(Percent::from(10), Percent::from(10));
+    EXPECT_EQ(Percent::from(10), Percent::from100ths(1000));
+    EXPECT_NE(Percent::from(10), Percent::from100ths(1001));
+    EXPECT_LT(Percent::from(10), Percent::from100ths(1001));
+    EXPECT_LE(Percent::from(10), Percent::from100ths(1000));
+    EXPECT_GT(Percent::from100ths(1001), Percent::from(10));
+    EXPECT_GE(Percent::from100ths(1000), Percent::from(10));
 }
