75c1bbeee3f078bd8039eb9ffaca4eea5b26a776
Piotr Kugla <piku235@gmail.com>
2026-08-02 16:35:24 +0200

    Drop kEventName from DomainEvent

--- src/common/domain/DomainEvent.h
+++ src/common/domain/DomainEvent.h
@@ -3,8 +3,6 @@
 namespace mobmatter::common::domain {
 
 struct DomainEvent {
-    static constexpr char kEventName[] = ""; // override by shadowing it
-
     virtual ~DomainEvent() = default;
     virtual const char* eventName() const = 0;
 };
